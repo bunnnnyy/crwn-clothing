8d8653dc23a63870275f2f4cbc8399744de2602f
bunnnnyy <vikasvickycoorg@gmail.com>
2020-12-27 11:19:54 +0530

    added header component and link to switch between shoppage and homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import HomePage from './pages/Homepage/homepage.component';
-import Shoppage from './pages/shop/shoppage.component'
+import Shoppage from './pages/shop/shoppage.component';
+import Header from './components/header/Header.component'
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div >
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={Shoppage} />
