3c8b06826de78a056a2db0cea33baa5e68f7c13f
bunnnnyy <vikasvickycoorg@gmail.com>
2021-04-19 13:03:08 +0530

    shop data is an array , bug fix

--- src/components/collections-overview/collectionOverview.component.jsx
+++ src/components/collections-overview/collectionOverview.component.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 import Collectionpreview from "../Collection-preview/Collection.Preview.component";
 
 import "./collectionOverview.style.scss";
 
-const CollectionOverview = ({ collections }) => (
-  <div className="collections-overview">
-    {collections.map(({ id, ...othercollectionprops }) => (
-      <Collectionpreview key={id} {...othercollectionprops} />
-    ))}
-  </div>
-);
+const CollectionOverview = ({ collections }) => {
+  console.log("collections", collections);
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...othercollectionprops }) => (
+        <Collectionpreview key={id} {...othercollectionprops} />
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
