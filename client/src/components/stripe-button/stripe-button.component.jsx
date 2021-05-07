b6e0430fa98221f7f4236227a79308deba796f7d
bunnnnyy <vikasvickycoorg@gmail.com>
2021-05-07 20:25:43 +0530

    buf fixed with stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -4,9 +4,10 @@ import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = "pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H";
+  const publishableKey =
+    "pk_test_51IlbAnSCZcZQMjATYHdisuq5ZJpqpDtjMpbddNaUFXoAxQFjUQDfLjxVkqHILtqVgtbCmd6AnDmjtIqH19jfIn3H00vB9UbIrQ";
 
-  const onToken = (token) => {
+  const onToken = ({ token }) => {
     axios({
       url: "payment",
       method: "post",
@@ -33,7 +34,7 @@ const StripeCheckoutButton = ({ price }) => {
       billingAddress
       shippingAddress
       image="https://svgshare.com/i/CUz.svg"
-      description={`Your total is $${price}`}
+      description={`Your total is Rs.{price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
       token={onToken}
